1d954fd1845a7aee433a4963688f34ce58f2d720
Monisha Ishwar <monishamoger15@gmail.com>
2025-09-18 22:24:18 +0530

    Add project files.

---
+++ Customers.aspx
@@ -0,0 +1,124 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Customers.aspx.cs" Inherits="WebAppSample.Customers" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="bootstrap.css" rel="stylesheet" />
+     <script>
+         function validate() {
+             let name = document.getElementById("txtName").value;
+             let address = document.getElementById("txtAdd").value;
+             let accountSelected = document.getElementById("ddlAccounts").SelectedIndex;
+
+             if (name.trim().length ==0) {
+                 document.getElementById("lblMsg").innerHTML = "Enter Your Name Please";
+                 document.getElementById("lblMsg").style = "color:red";
+                 return false
+             }
+             else if (address.trim().length == 0) {
+                 document.getElementById("lblMsg").innerHTML = "Enter Your Address Please";
+                 document.getElementById("lblMsg").style = "color:red";
+                 return false;
+             }
+            /* else if (accountSelected.length == 0) {
+                 document.getElementById("lblMsg").innerHTML = "Please Choose Your Account Type";
+                 document.getElementById("lblMsg").style = "color:red";
+                 return false;
+             }*/
+         }
+     </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div style="width:100%; border:solid thin black; padding:20px;">
+                <center>
+                    <h1>Customer  Managemnet</h1>
+                </center>
+            </div>
+            <hr />
+
+            <div>
+                <div style="width:40%; border:solid thin black; padding:20px;  display:inline-block" >
+                    <div >
+                        Account No: 
+                        <asp:Label ID="lblAccNum" runat="server" Text="Label"></asp:Label>
+                         
+                    </div><br />
+                    <div>Name : 
+                        <asp:TextBox ID="txtName" runat="server" CssClass="form-control" ></asp:TextBox>
+                         
+                    </div><br />
+
+                    <div>Address : 
+                        <asp:TextBox ID="txtAdd" runat="server" CssClass="form-control"></asp:TextBox>
+                         
+                    </div><br />
+                    <div>Preferred Account : 
+                        <asp:DropDownList ID="ddlAccounts" runat="server" CssClass="form-control" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="264px"></asp:DropDownList>
+                               
+                               <br />
+                    </div><br />
+
+                    <div>Activate Account : 
+                        <asp:CheckBox ID="cbStatus" runat="server" CssClass="form-control" OnCheckedChanged="CheckBox1_CheckedChanged" />
+                        <br />
+                    </div><br />
+                    <div>
+
+                    <div>
+                        <asp:Button ID="btnCancel" runat="server" Text="Cancel"  CssClass="btn btn-warning form-control" style="padding:10px; background-color:red; margin-right:50px" Width="146px" OnClick="btnCancel_Click" />
+                        <asp:Button ID="btnSave" runat="server" Text="Save"  OnClientClick="return validate()" cssClass="btn btn-primary form-control"  style="background-color:green; margin-left:20px; padding:10px" Width="135px" OnClick="Button3_Click" />
+                        <br />
+                    </div>
+ 
+                    </div>
+                      <div>
+                        <asp:Label ID="lblMsg" runat="server" Text=" "></asp:Label>
+                    </div><br />
+
+                </div>
+                <div style="width:55%; border:solid thin black; margin-top:20px; padding:10px; display:inline-block">
+                    <div>Enter Customer Name: 
+                        <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+
+                        <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="Button2_Click" />
+                        <p> <asp:Label ID="lblSearchMsg" runat="server" Text=" "></asp:Label></p>
+                    </div>
+                    <br />
+                    <div>
+                        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" OnRowCommand="GridView1_RowCommand">
+                            <AlternatingRowStyle BackColor="White" />
+                            <Columns>
+                                <asp:TemplateField HeaderText="Actions">
+                                    <ItemTemplate>
+                                        <asp:LinkButton ID="btnEdit" runat="server" CommandArgument='<%#Eval("Accno")%>' CommandName="EditRow" OnCommand="btnEdit_Click">Edit</asp:LinkButton>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                            </Columns>
+                            <EditRowStyle BackColor="#2461BF" />
+                            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                            <RowStyle BackColor="#EFF3FB" />
+                            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+                        </asp:GridView>
+                    </div>
+
+                </div>
+            </div>
+            <div>
+                <p style="text-align:center; padding:20px; margin-top:10px">Copy Rights &copy; All Rights are Reserved to Tech Novice Solutions, Mysore.</p>
+            </div>
+        </div>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ Customers.aspx.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+
+namespace WebAppSample
+{
+    public partial class Customers : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection();
+        int status = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial catalog=Banking; integrated security=true";
+            if (!IsPostBack)
+            {
+                ddlAccounts.DataSource = Accounts();
+                ddlAccounts.DataTextField = "Account";
+                ddlAccounts.DataValueField = "Id";
+                ddlAccounts.DataBind();
+                lblAccNum.Text = AccountNumber().ToString();
+                ViewState["action"] = 1;
+            }
+
+        }
+        int AccountNumber()
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            SqlCommand cmdAccnum = new SqlCommand("Select max(accno)+1 from Customers", con);
+            return Convert.ToInt32(cmdAccnum.ExecuteScalar());
+
+        }
+
+        DataTable Accounts()
+        {
+            SqlDataAdapter daActs = new SqlDataAdapter("Select Id,Account from Accounts", con);
+            DataTable dtActs = new DataTable();
+            daActs.Fill(dtActs);
+            DataRow r = dtActs.NewRow();
+            r[0] = -1;
+            r[1] = "Choose Preferred Account";
+            dtActs.Rows.InsertAt(r, 0);
+            return dtActs;
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(ddlAccounts.SelectedValue) == -1)
+            {
+                lblMsg.Text = "Please Choose Your Account Type";
+                lblMsg.ForeColor = Color.Red;
+                return;
+            }
+
+            if (!cbStatus.Checked)
+            {
+                status = 0;
+            }
+            SqlCommand cmdSave = new SqlCommand();
+            cmdSave.Connection = con;
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            if (Convert.ToInt32(ViewState["action"]) == 1)
+            {
+                cmdSave.CommandText = "insert into customers values(@accno, @name, @address, @account, @status)";
+            }
+            else if (Convert.ToInt32(ViewState["action"]) == 2)
+            {
+                cmdSave.CommandText = "update customers set name=@name, address=@address, account=@account, status=@status where accno=@accno";
+            }
+
+            cmdSave.Parameters.AddWithValue("@accno", Convert.ToInt32(lblAccNum.Text));
+            cmdSave.Parameters.AddWithValue("@name", txtName.Text.Trim());
+            cmdSave.Parameters.AddWithValue("@address", txtAdd.Text.Trim());
+            cmdSave.Parameters.AddWithValue("@account", Convert.ToInt32(ddlAccounts.SelectedValue));
+            cmdSave.Parameters.AddWithValue("@status", status);
+
+            try
+            {
+                int recordsAffected = cmdSave.ExecuteNonQuery();
+
+                if (recordsAffected > 0)
+                {
+                    if (Convert.ToInt32(ViewState["action"]) == 1)
+                    {
+                        lblMsg.Text = "Account has been opened for: " + txtName.Text.Trim();
+
+                    }
+                    else if (Convert.ToInt32(ViewState["action"]) == 2)
+                    {
+                        lblMsg.Text = "Account details for the Account Number: " + lblAccNum.Text.Trim() + " has been changed";
+                        BindGrid();
+                    }
+                    btnSave.Enabled = false;
+                    lblMsg.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblMsg.Text = "Failed to Add/ Modify the deatils";
+                    lblMsg.ForeColor = Color.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = "Database Error: " + ex.Message;
+                lblMsg.ForeColor = Color.Red;
+            }
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            ViewState["action"] = 1;
+            status = 1;
+            btnSave.Enabled = true;
+            txtName.Text = " ";
+            txtAdd.Text = "";
+            ddlAccounts.SelectedIndex = 0;
+            lblMsg.Text = "";
+            lblAccNum.Text = AccountNumber().ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+        void BindGrid()
+        {
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+
+            SqlDataAdapter daSearch = new SqlDataAdapter("Select c.Accno, c.Name, c.Address, a.Account, Case WHEN c.status = 1 THEN 'Active' else 'De-Activated' END as [Status] FROM Customers c INNER JOIN Accounts a ON a.id = c.account WHERE c.name LIKE '%' + @name + '%'", con);
+
+            daSearch.SelectCommand.Parameters.AddWithValue("@name", txtSearch.Text.Trim());
+
+            DataTable dtCust = new DataTable();
+            daSearch.Fill(dtCust);
+            GridView1.DataSource = dtCust;
+            GridView1.DataBind();
+            lblSearchMsg.Text = dtCust.Rows.Count + " Record(s) Found";
+            lblSearchMsg.ForeColor = Color.Blue;
+        }
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            LinkButton btn = (LinkButton)sender;
+            GridViewRow rSelected = (GridViewRow)btn.Parent.Parent;
+            lblAccNum.Text = rSelected.Cells[1].Text;
+            txtName.Text = rSelected.Cells[2].Text;
+            txtAdd.Text = rSelected.Cells[3].Text;
+            ListItem accountSelected = ddlAccounts.Items.FindByText(rSelected.Cells[4].Text);
+            ddlAccounts.ClearSelection();
+            accountSelected.Selected = true;
+
+            if (rSelected.Cells[5].Text == "Active")
+            {
+                cbStatus.Checked = true;
+            }
+            else
+            {
+                cbStatus.Checked = false;
+            }
+
+            ViewState["action"] = 2;
+        }
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+        }
+    }
+}
